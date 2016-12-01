b9c2090be2a4902fbb5af9a1c6bc9f65117a9ffc
Spiros Mavroidakos <spirosmavroidakos@gmail.com>
2016-11-30 23:12:26 -0500

    Made the changes demanded by the prof

--- Assignment4/q3_chat_botV2.py
+++ Assignment4/q3_chat_botV2.py
@@ -1,86 +1,101 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 #Author Spiros Mavroidakos
 #Version 3.0
 ''' Solution to Assignment 4 question 3 chat bot'''
 import os
+import re, string
 import sys
 import random
 from operator import itemgetter
 
 wordFreq = {}
 endWord = []
+regex = re.compile('[^a-zA-Z0-9]')
 #This function will save each word pair to the wordFreq dictionary
 def database(path):
 	if os.path.exists(path) == False:
-		print("File does not exist. Please enter existing file.")
+		print("File does not exist. Try Again with existing file.")
 		return None
 	file = open(path,"r")
+	regex = re.compile('[^a-zA-Z0-9]')
 	wholetext = file.read().split()
 	file.close();
-	prevword = wholetext[0].lower()
+	prevword = regex.sub("",wholetext[0].lower())
 	del wholetext[0]
 	for word in wholetext:
 		currentWord = word.lower()
-		newpair = prevword + " " + currentWord
+		if word[len(word)-1] == ".":
+			newpair = prevword + " " + word.lower()
+		else:
+			newpair = prevword + " " + regex.sub("", word.lower())
 		if "-" in currentWord:
 			currentWord = currentWord.split("-")
 			for i in currentWord:
-				temp = prevword + " " + i
-				if temp not in wordFreq:
-					wordFreq[temp] = 1
-					prevword = i
-				else:
-					wordFreq[temp] +=1
-					prevword = i
+				if len(i) > 0:
+					if i[len(i)-1] == "":
+						temp = prevword + " " + i
+					else:
+						temp = prevword + " " + regex.sub("", i)
+					if temp not in wordFreq and regex.sub("",i) != "":
+						wordFreq[temp] = 1
+						prevword = regex.sub("", i)
+					elif regex.sub("",i) != "":
+						wordFreq[temp] +=1
+						prevword = regex.sub("", i)
 		elif newpair not in wordFreq:
 			wordFreq[newpair] = 1
-			prevword = currentWord
+			prevword = regex.sub("", currentWord)
 		else:
 			wordFreq[newpair] += 1
-			prevword = currentWord
-	endWord.append(prevword)
+			prevword = regex.sub("", currentWord)
 def findPair(word):
 	count = 0
-	string = word
-	comparison = ""
-	for key in wordFreq:
-		comparison = ""
-		if count == 10 :
-			break
-		for char in key:
-			comparison += char 
-			if (string == comparison) and ((key[len(comparison)] == " ") or (key[len(comparison)] == ".")) :
-				if (count == 0):
-					print(key.capitalize(), end = " ")
-				else:
-					print(key.lower(), end = " ")
-				count += 1
-				if key[len(key)-1] == ".":
-					return None
-			elif (char == " "):
-				comparison = ""
+	string = word.lower()
+	comparison = [] 
+	while True:
+		for key in wordFreq:
+			comparison = key.split(" ")
+			if count == 20:
 				break
+			if comparison[0].lower() == string:
+				if (count == 0):
+					print comparison[1].capitalize(), 
+					string = comparison[1].lower()
+				else:
+					print comparison[1].lower(),
+					string = comparison[1].lower()
+				count += 1
+				if comparison[1][len(comparison[1])-1] == ".":
+					return None
+				if count == 20:
+					print ".",
+					return None
+		if count == 0 :
+			break
+			
 	if count == 0:
 		
 		if (word + ".") in endWord:
-			print(word.capitalize(), end = ".")
-			return None 
-		tmp = random.choice(list(wordFreq.keys())).capitalize()
-		if tmp[len(tmp)-1] == ".":
-			print(tmp, end = "")
-		else:
-			print(tmp, end = ".")
-		count = 0
-		return None
-	else: 
-		while(count < 10):
-			string = random.choice(list(wordFreq.keys())).capitalize()
-			print(string.lower(), end = " ")
-			count += 1
-			if string[len(string)-1] == ".":
-				return None
-	if count == 10 :
-		print(".", end = "")
+			print(word.capitalize(),)
+			return None  
+		tmp = random.choice(list(wordFreq.keys())).split(" ")
+		string = tmp[1]
+		while count != 20:
+			for ite in wordFreq:
+				comparison = ite.split(" ")
+				if comparison[0].lower() == string:
+					if (count == 0):
+						print comparison[1].capitalize(), 
+						string = comparison[1].lower()
+					else:
+						print comparison[1].lower(),
+						string = comparison[1].lower() 
+					count += 1
+					if comparison[1][len(comparison[1])-1] == ".":
+						return None
+					if count == 20:
+						print ".",
+						return None 
 		
 #Checks to see if at least one text source has been entered
 if len(sys.argv)<= 1:
@@ -91,10 +106,11 @@ del sys.argv[0]
 for word in sys.argv:
 	database(word)
 while True:
-	userInput = input("Send: \t")
+	userInput = raw_input("Send: \t")
+	userInput = regex.sub("", userInput)
 	wordToCompare = userInput.split(" ")
 	word = wordToCompare[len(wordToCompare) - 1]
-	print("Received: \t", end = "")
+	print "Received: \t", 
 	findPair(word)
 	print("")
 #for key,val in sorted(wordFreq.items(), key = itemgetter(1), reverse = True):
