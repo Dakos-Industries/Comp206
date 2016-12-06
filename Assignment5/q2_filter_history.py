3274fe09adfd673d55036d03bd58c58457721bdf
Spiros Mavroidakos <spirosmavroidakos@gmail.com>
2016-12-05 21:32:37 -0500

    finished assignment

--- Assignment5/q2_filter_history.py
+++ Assignment5/q2_filter_history.py
@@ -9,18 +9,33 @@ import ctypes
 import sys
 import pickle
 import os
+# List to save the image states
+historia = []
 # Same filtering method as done in Q1
 def filt():
 	# check if the result image exists
 	if os.path.isfile("result.bmp") == False:
 		print "No file loaded! Please load a file."
 		return None
+	f = open('history.pickle','rb')
+	historia = pickle.load(f)
+	pos = pickle.load(f)
+	f.close()
+	if pos != 0 and len(historia) != 0:
+		pos += 1	
+		del historia [pos:len(historia)]
+	else:
+		pos = 0
+		del historia [pos:len(historia)]
 	# Import the shared library to perform the filter
 	fastFilt = ctypes.cdll.LoadLibrary("./libfast_filter.so")
 	#Open the image, get the data and send the data to the undo pickle dump file
 	in_img = open('result.bmp', 'rb')
 	in_img_data = in_img.read()
-	pickle.dump(in_img_data, open("undo.p","wb"))
+	if pos  >= len(historia):
+		historia.append(in_img_data)
+	else:
+		historia[pos] = in_img_data
 	in_img.close()
 
 	width = (int)(sys.argv[2])
@@ -42,6 +57,13 @@ def filt():
 	out_img = open("result.bmp", "wb")
 	out_img.write(out_img_data)
 	out_img.close()
+	if pos == 0:
+		pos += 1
+	f = open('history.pickle','wb')
+	pickle.dump(historia,f)
+	pickle.dump(pos,f)
+	f.close()
+	print pos, len(historia)
 def load():
 	#Load in the data from the given bmp file
 	in_img = open(sys.argv[2], 'rb')
@@ -50,43 +72,69 @@ def load():
  	out_img = open("result.bmp", "wb")
 	out_img.write(in_img_data)
 	out_img.close()
+	pos = 0
 	# Delete the old pickle dump files if any exist
-	if os.path.isfile("undo.p") == True:
-		os.remove("undo.p")
-	if os.path.isfile("redo.p") == True:
-		os.remove("redo.p")
+	if os.path.isfile("history.pickle") == True:
+		os.remove("history.pickle")
+	f = open('history.pickle','wb')
+	pickle.dump(historia,f)
+	pickle.dump(pos,f)
+	f.close()
+
 def undo():
 	#cheching if undo was done before
-	if os.path.isfile("undo.p") == False:
+	if os.path.isfile("history.pickle") == False:
 		print "Nothing to undo!"
 		return None
-	# load the previous data and undo the previous steps
-	undo_data = pickle.load(open("undo.p","rb"))
-
-	redo = open('result.bmp', 'rb')
-	redo_data = redo.read()
-	pickle.dump(redo_data, open("redo.p","wb"))
-	redo.close()
-
-	out_img = open("result.bmp", "wb")
-	out_img.write(undo_data)
-	out_img.close()
-def redo():
-	# checking to see if redo is possible
-	if os.path.isfile("redo.p") == False:
-		print "Nothing to redo!"
+	f = open('history.pickle','rb')
+	historia = pickle.load(f)
+	pos = pickle.load(f)
+	print pos
+	f.close()
+	if pos == 0:
+		print "Can't undo at earliest change"
 		return None
-	# load the data and perform the redo
-	redo_data = pickle.load(open("redo.p","rb"))
-
-	undo = open('result.bmp', 'rb')
-	undo_data = undo.read()
-	pickle.dump(undo_data, open("undo.p","wb"))
-	undo.close()
-
-	out_img = open("result.bmp", "wb")
-	out_img.write(redo_data)
-	out_img.close()
+	else:
+		
+		# load the previous data and undo the previous steps
+		redo = open('result.bmp', 'rb')
+		redo_data = redo.read()
+		if pos == len(historia):
+			historia.append(redo_data)
+		else:
+			historia[pos] = redo_data
+		pos -= 1
+		print pos, len(historia)
+		out_img = open("result.bmp", "wb")
+		out_img.write(historia[pos])
+		out_img.close()
+		f = open('history.pickle','wb')
+		pickle.dump(historia,f)
+		pickle.dump(pos,f)
+		f.close()
+def redo():
+	#cheching if undo was done before
+	if os.path.isfile("history.pickle") == False:
+		print "Nothing to undo!"
+		return None
+	f = open('history.pickle','rb')
+	historia = pickle.load(f)
+	pos = pickle.load(f)
+	f.close()
+	if pos+1 >= (len(historia)):
+		print "At latest change"
+		return None
+	else:
+		
+		# load the previous data and undo the previous steps
+		pos += 1
+		out_img = open("result.bmp", "wb")
+		out_img.write(historia[pos])
+		out_img.close()
+		f = open('history.pickle','wb')
+		pickle.dump(historia,f)
+		pickle.dump(pos,f)
+		f.close()
 
 
 # checking user input	
